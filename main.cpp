8ca7bb47aee5a8c704e7f6ea0628f3a6a7d2439f
karshtharyani <karsht8@gmail.com>
2020-11-13 21:49:06 -0500

    removing gameState

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include "Game.hpp"
 int main() {
   using namespace game;
-  GameState gameState = inProgress;
   Board board{
       {empty, empty, empty}, {empty, empty, empty}, {empty, empty, empty}};
   return 0;
