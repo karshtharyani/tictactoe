bb30986e3006d3f960ea73e48afb6862c9ce1285
karshtharyani <karsht8@gmail.com>
2020-11-13 22:21:35 -0500

    Calling glade gui in main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include "Game.hpp"
-int main() {
+#include <gtkmm.h>
+int main(int argc, char *argv[]) {
   using namespace game;
+  using namespace Gtk;
+  using namespace Glib;
+  gWindow *pWindow = nullptr;
+  auto app = gApplication::create(argc, argv, "org.gtkmm.example");
+  gRefPtr<gBuilder> builder =
+      gBuilder::create_from_file("tictactoegameUI.glade");
+  builder->get_widget("TicTacToe", pWindow);
   Board board{
       {empty, empty, empty}, {empty, empty, empty}, {empty, empty, empty}};
+  app->run(*pWindow);
+  delete pWindow;
   return 0;
 }
